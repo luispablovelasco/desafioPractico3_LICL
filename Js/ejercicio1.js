756499f0099c3df31db249ba77358b67a5111011
Pablo Velasco <pablo.velasco@gmail.com>
2022-11-10 10:57:05 -0600

    interfaz ejercicio 1

---
+++ Js/ejercicio1.js
@@ -0,0 +1,18 @@
+//Obteniendo la referencia del boton calcular
+const btnSumar = document.getElementById("btnCalcular");
+const btnRestar = document.getElementById("btnRestar");
+const btnDividir = document.getElementById("btnDividir");
+const btnMultiplicar = document.getElementById("btnMultiplicar");
+
+
+//Definimos la clase  calculadora
+
+const calculadora  = function (Valor1, Valor2, Resultado) {
+    //Propiedades de la clase
+    this.Valor1 = parse.float(Valor1);
+    this.Valor2 = parse.float(Valor2);
+    this.Resultado = parse.float(Resultado);
+
+    //Metodos de la clase
+    this.sumar = new Function(Resultado = Valor1 + Valor12)
+}
